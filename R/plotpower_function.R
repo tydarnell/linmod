451c1ce3d54721ef52ad5abe6aadc87545a40a4a
Ty Darnell <tydarnell@gmail.com>
2019-04-11 16:58:22 -0400

    Update plotpower_function.R

--- R/plotpower_function.R
+++ R/plotpower_function.R
@@ -7,7 +7,12 @@
 #' @export
 #' @examples
 #' plotpower_function()
+
 plotpower=function(n,a){
+  if (!requireNamespace("ggplot", quietly = TRUE)) {
+    stop("Package \"ggplot2\" needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
   f=powerfun(n,a)
   p = ggplot(data = data.frame(x = 0), mapping = aes(x = x))
   p+stat_function(fun= f,color="blue")+scale_x_continuous(limits = c(0, 1))+xlab(expression(theta))+ylab("Power")
