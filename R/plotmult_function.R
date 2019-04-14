1b43101e060e3673fa6c614d9b8a8393bca18bf7
Ty Darnell <tydarnell@gmail.com>
2019-04-14 17:19:33 -0400

    update

--- R/plotmult_function.R
+++ R/plotmult_function.R
@@ -16,7 +16,7 @@ plotmult <- function(v,a,lower=0,upper=1) {
   d[[i]] =createpower(v[i],a)
   }
 
-  a=map(d,function(f) ggplot2::stat_function(fun=f,color=randomcoloR::randomColor()))
+  a=purrr::map(d,function(f) ggplot2::stat_function(fun=f,color=randomcoloR::randomColor()))
   p = ggplot2::ggplot(data = data.frame(x = 0), mapping = ggplot2::aes(x = x))
   p+a+ggplot2::scale_x_continuous(limits=c(lower,upper))
 }
