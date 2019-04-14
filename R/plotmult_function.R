d087fac1f7e251750e9771b71a89b112a6223707
Ty Darnell <tydarnell@gmail.com>
2019-04-14 16:39:11 -0400

    plotmult

---
+++ R/plotmult_function.R
@@ -0,0 +1,21 @@
+#' Plot Mult Function
+#'
+#' Plots several functions
+#' @param v vector of values
+#' @param a alpha value
+#' @param lower lower bound
+#' @param upper upper bound
+#' @keywords plotfunc
+#' @export
+#' @examples
+#' plotmult_function()
+
+plotmult <- function(v,a,lower=0,upper=1) {
+  d=list(length=length(v))
+  for (i in seq_along(v)) {
+  d[[i]] =createpower(v[i],a)
+  }
+  a=map(d,function(f) ggplot2::stat_function(fun=f))
+  p = ggplot2::ggplot(data = data.frame(x = 0), mapping = ggplot2::aes(x = x))
+  p+a+scale_x_continuous(limits=c(lower,upper))
+}
